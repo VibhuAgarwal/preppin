eeed6917f55aa5edf9a6708af8e681300d91416e
vibhragrwl <vibhor@transfi.com>
2025-03-23 19:02:35 +0530

    Added dummt sections an dstructure of how page will look like

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const RootLayout = ({children}:{children: React.ReactNode}
 ) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href='/' className='flex items-center gap-2'>
+          <Image src='/logo.svg' alt='Preppin logo' height={32} width={38} />
+          <h2 className='text-primary-100'>Preppin</h2>
+      </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,36 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg capitalize">
+          <h2 className='text-lg'>Get Interview Ready with AI powered Practice</h2>
+          <p>Practice on real interview questions and get instant feedback</p>
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href='/interview'>Start an Interview</Link>
+            </Button>
+        </div>
+        <Image src='/robot.png' alt='robo-dude image' width={400} height={400}className='max-sm:hidden' />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        Your Interviews
+        <div className='interview-section'>
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take and interview</h2>
+        <div className="interviews">
+          <p>There are no interviews available...</p>
+        </div>
+      </section>
+    </>
   )
 }
 
